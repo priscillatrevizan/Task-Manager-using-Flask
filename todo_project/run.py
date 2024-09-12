08aeb01b24a0cc6d5a961bb6edf7a813341cedb9
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2024-09-12 00:22:07 +0000

    Fix: Update host binding to use environment variables and prevent hardcoded secrets

--- todo_project/run.py
+++ todo_project/run.py
@@ -1,5 +1,5 @@
-from todo_project import app
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+    app.run(host=host, port=5000, debug=False)
