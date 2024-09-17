e99d727e9b08e354de0f76e844f53f71cef14d8f
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2024-09-17 18:25:30 -0300

    fix: bandit bug  adjusted

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,16 @@
 
 # Espera o Graylog estar disponível
 echo "Esperando pelo Graylog..."
-for i in {1..6}; do  # Espera até 1 minuto (6 * 10s)
-  if curl -s -o /dev/null -w "%{http_code}" http://192.168.3.75:9000/api/system | grep -q "200"; then
-    echo "Graylog está disponível. Iniciando a aplicação Flask..."
-    break
-  else
+until curl -s -o /dev/null -w "%{http_code}" http://192.168.3.75:9000/api/system | grep "200"; do
+    echo "Tentando acessar Graylog..."
     echo "Aguardando Graylog estar disponível..."
     sleep 10
-  fi
 done
 
-# Verifica se o Graylog está acessível após 1 minuto
-if [ $i -eq 6 ]; then
-  echo "Erro: Graylog não está disponível após 1 minuto. Encerrando."
-  exit 1
-fi
+echo "Graylog está disponível. Iniciando a aplicação Flask..."
 
 # Inicia o Flask
 exec python3 ./todo_project/run.py
+
 
 
